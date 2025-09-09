056e8b4ff2ed16cc5d8887f0328bfc5f530f6907
rahulsrivas <114463829+rahulsrivas@users.noreply.github.com>
2025-09-09 14:56:15 +0530

    adding VM resource and tags

--- main.tf
+++ main.tf
@@ -18,3 +18,26 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = true
   project                 = var.project_id
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+  project      = var.project_id
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+access_config {
+  
+}
+  }
+ tags = ["web", "dev"]
+}
